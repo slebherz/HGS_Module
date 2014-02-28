f2b392ef91ab95eb1f94d32159e831da99cac9d2
slebherz <slebherz@hydrologics.net>
2014-02-28 11:01:11 -0500

    Modified the module for explicit presolve/postsolve invocation.
       - Added sample OCL usage of the module

--- code/HGS_step.cpp
+++ code/HGS_step.cpp
@@ -31,18 +31,29 @@ void ReadHGSInput(float argument[], std::string fnameHGS);
 extern "C" __declspec( dllexport )
     short  MODULE_STEP(float argument[])
 {   
-	WriteOasisOutput(argument);
-	
-	// The absolute period is used to identify the files written by HGS for
-	// the current timestep -- files are written with a .[timestep#] extension.
-	int period = (int)argument[0];
-	std::ostringstream fnameHGS;
-	fnameHGS << ::runDirectory << "/HGS/" << "LRRBo.OASIS_output."
-		<< std::setfill('0') << std::setw(3) << period;	
-	
-	WaitForHGS(period, fnameHGS.str());
+	bool isPreSolve = argument[0] == 0;
+	int period = (int)argument[1];
 
-	ReadHGSInput(argument, fnameHGS.str());
+	// The models run independently for the first timestep.
+	if(period == 1) return 1;
+
+	if(isPreSolve)
+	{
+		// The absolute period *of the previous timestep* is used to identify
+		// the files written by HGS to be incorporated into the current OASIS
+		// timestepfor -- files are written with a .[timestep#] extension.
+		std::ostringstream fnameHGS;
+		fnameHGS << ::runDirectory << "/HGS/" << "LRRBo.OASIS_output."
+		<< std::setfill('0') << std::setw(3) << period - 1;	
+
+		WaitForHGS(period, fnameHGS.str());
+
+		ReadHGSInput(argument, fnameHGS.str());
+	}
+	else
+	{
+		WriteOasisOutput(argument);
+	}
 
     return 1;
 }
@@ -56,21 +67,21 @@ void WriteOasisOutput(float argument[])
 	// the call(s) to run the module in OCL.
 	//
 	// The period is used to generate *.[period#] file extensions for the output.
-	int period = (int)argument[0];
+	int period = (int)argument[1];
 	// Deliveries (in cms, negative values) to all demands in the model.
-	double delivery350 = argument[1];
-	double delivery650 = argument[2];
-	double delivery651 = argument[3];
-	double delivery652 = argument[4];
-	double delivery850 = argument[5];
-	double delivery950 = argument[6];
-	double delivery951 = argument[7];
+	double delivery350 = argument[2];
+	double delivery650 = argument[3];
+	double delivery651 = argument[4];
+	double delivery652 = argument[5];
+	double delivery850 = argument[6];
+	double delivery950 = argument[7];
+	double delivery951 = argument[8];
 	// Releases (in cms) from all reservoirs in the model.
-	double flow295_290 = argument[8];
-	double flow395_390 = argument[9];
-	double flow995_980 = argument[10];
+	double flow295_290 = argument[9];
+	double flow395_390 = argument[10];
+	double flow995_980 = argument[11];
 	// Boolean that identifies whether MDS is met for the timestep.
-	bool MDS_IsMet = argument[11];
+	bool MDS_IsMet = argument[12];
 
 	// Prepare the file that will contain the deliveries.
 	std::stringstream fnameOasisDeliveries;
@@ -112,7 +123,7 @@ void WriteOasisOutput(float argument[])
 	fOasisMDS.open(fnameOasisMDS.str());
 	fOasisMDS.precision(0);
 	// Write the MDS flag.
-	fOasisMDS << std::fixed << MDS_IsMet << "\t\t!1: MDS satisfiied, 0: MDS not satisfied" << std::endl;
+	fOasisMDS << std::fixed << MDS_IsMet << "\t\t!1: MDS satisfied, 0: MDS not satisfied" << std::endl;
 	fOasisMDS.close();
 }
 
@@ -125,8 +136,7 @@ void WaitForHGS(int period, std::string fnameHGS)
 	// Wait here (prevent OASIS from continuing) until:
 	//	a. HGS reads the OASIS output for the current timestep.
 	//	b. HGS solves for the current timestep.
-	//	c. HGS writes output for the current timestep to the next
-	//	   timestep) until HGS has read the data for the current timestep.
+	//	c. HGS writes output for the current timestep.
 	while(FileExists(fnameHGS) == false)
 	{
 		// This Oasis function checks whether the user has hit the [X] button
@@ -150,34 +160,28 @@ void ReadHGSInput(float argument[], std::string fnameHGS)
 {
 	// The absolute period is used to write the .[per#] extension on files
 	// to be read by HGS.
-	int period = (int)argument[0];
+	int period = (int)argument[1];
 
 	// Initialize the variables to store data written by HGS. The data is
 	// loaded onto the arguments array. Upon completion of the module
 	// execution, the values of the array are assigned to OCL Udefs that are
 	// specified in the OCL call(s) to run the module.
 	std::fill(argument, argument + sizeof(argument), 0);
-
-	// Parse the HGS output on timesteps except for the very first. The models
-	// run independently for the first timestep.
-	if(period > 1)
+	std::ifstream file(fnameHGS);
+	std::string line;
+	int idx = 0;
+	// Each line in the file contains data in the expected format:
+	// [value]\t![description]
+	// 10.26	!flow for node 100 (m^3/s)
+	while(std::getline(file, line))
 	{
-		std::ifstream file(fnameHGS);
-		std::string line;
-		int idx = 0;
-		// Each line in the file contains data in the expected format:
-		// [value]\t![description]
-		// 10.26	!flow for node 100 (m^3/s)
-		while(std::getline(file, line))
-		{
-			std::stringstream lineStream(line);
-			std::string valueString;
-			double value;
-			// Read the value from the line ('>>' stops at the first whitespace).
-			lineStream >> value;
-			// Store the inflow adjustment (flow, m^3/s).
-			argument[idx] = value;
-			++idx;
-		}
+		std::stringstream lineStream(line);
+		std::string valueString;
+		double value;
+		// Read the value from the line ('>>' stops at the first whitespace).
+		lineStream >> value;
+		// Store the inflow adjustment (flow, m^3/s).
+		argument[idx] = value;
+		++idx;
 	}
 }
